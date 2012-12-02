0b6491ac36fc1311162cb79a75958a19ad683b9c
Samuel Clay <samuel@ofbrooklyn.com>
2012-12-01 19:20:54 -0800

    Moving www to prod.

--- donationparty/settings.py
+++ donationparty/settings.py
@@ -10,7 +10,7 @@ ADMINS = (
 )
 
 MANAGERS = ADMINS
-PREPEND_WWW = True
+
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name

2d13174ff0bf806b7821e27aa6809697a3580f21
Ely <elylerner@gmail.com>
2012-12-01 22:05:12 -0800

    oops

--- donationparty/views.py
+++ donationparty/views.py
@@ -61,7 +61,7 @@ def donation_create(request):
 
 def invite_emails(request):
     round = get_object_or_404(Round, url=request.POST['round_id'])
-    invites = Request.POST['invites']
+    invites = request.POST['invites']
     Emailer.email_invitees(round.absolute_url(), round.donations,
                                round.expire_time, invites)
     

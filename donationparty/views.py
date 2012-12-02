aa91b55e7a85e4736849ed8289e3797d9848e0bc
Ely <elylerner@gmail.com>
2012-12-01 22:13:58 -0800

    it's late and I'm hung over

--- donationparty/views.py
+++ donationparty/views.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import get_object_or_404, render_to_response
 from django.template import RequestContext

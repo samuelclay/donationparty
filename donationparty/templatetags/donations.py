ee054e8d039d2d5ac271d23f63556d225c41f015
Samuel Clay <samuel@ofbrooklyn.com>
2012-12-08 19:50:45 -0800

    Adding donated flag.

--- donationparty/templatetags/donations.py
+++ donationparty/templatetags/donations.py
@@ -2,9 +2,13 @@ from django import template
 
 register = template.Library()
 
-@register.inclusion_tag('donations.xhtml')
+@register.inclusion_tag('donations.xhtml', takes_context=True)
 def render_donations(donations):
-    return {'donations': donations}
+    donated = context.get('request').COOKIES.get('donated_%s' % round.url)
+    return {
+        'donated': donated,
+        'donations': donations,
+    }
     
 @register.inclusion_tag('payment_info.xhtml', takes_context=True)
 def render_payment_info(context, round, donated=False, **kwargs):
